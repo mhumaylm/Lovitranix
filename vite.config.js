28c11dd13f3c1039af4ff058a353bc4933b57b59
Muhammad Humayl Malik <mhumaylm@gmail.com>
2025-10-22 20:40:39 +0500

    Fix sitemap namespace

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // MUST be '/' for custom domain
+  base: '/',  // Change this to root for custom domain
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
