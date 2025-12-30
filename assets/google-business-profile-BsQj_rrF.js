405b90da5e16c4431d7b93e35b10ad41cf625d63
Muhammad Humayl Malik <mhumaylm@gmail.com>
2025-12-31 01:53:55 +0500

    Updates

---
+++ assets/google-business-profile-BsQj_rrF.js
@@ -0,0 +1,1 @@
+import{j as e,H as s,F as o,R as r,a as t}from"./index-DrHZcpxd.js";import{S as a}from"./ServiceDetail-4mpiVVjL.js";const i=()=>e.jsxs(e.Fragment,{children:[e.jsx(s,{}),e.jsx(a,{serviceId:"google-business-profile"}),e.jsx(o,{})]});r.createRoot(document.getElementById("root")).render(e.jsx(t.StrictMode,{children:e.jsx(i,{})}));
